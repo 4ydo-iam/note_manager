4a986a1acff5aefc53330a1378ddced81375df2b
4ydo-iam <nemo81@mail.ru>
2024-12-23 14:29:52 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,15 @@
+print('Привет!')
+name = input('Введите имя пользователя:')
+print(f'Привет!{name} ')
+title = input(f'ввидите заголовок заметки уважаемый {name}:')
+print(f'заголовок называется : {title}' )
+print(f'продолжим {name}')
+content = input(f'ввидите описание заметки {name}:')
+print(f'прикрасно {name}, вы описали свою заметку: {title}')
+status = f"новая заметка: {title}"
+print(status)
+created_date = 'создана 22-12-2004'
+print(created_date)
+issue_date = 'закроется 31-12-2024'
+print(issue_date)
+print('Благодарим вас!')
