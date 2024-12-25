f27b4776a3be3de57a9a7c50a45ab45f75aa8858
4ydo-iam <nemo81@mail.ru>
2024-12-25 18:13:06 +0300

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,4 +1,6 @@
-created_date = ('21.12.2024')
+# делаем срез по строкам, убираем год
+
+created_date = '21.12.2024'
 print(created_date[:5])
-issue_date = ('25.12.2024')
+issue_date = '25.12.2024'
 print(issue_date[:5])
