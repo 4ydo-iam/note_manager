b21605481e7e04cccc03c37e996052ac50b0c88a
4ydo-iam <nemo81@mail.ru>
2024-12-25 18:12:19 +0300

    Add files via upload

--- add_list.py
+++ add_list.py
@@ -1,5 +1,7 @@
+#Создаём список
+
 title1 = input("ввидите название заголовка: ")
 title2 = input("ввидите название заголовка: ")
 title3 = input("ввидите название заголовка: ")
-title = [(title1),(title2),(title3)]
+title = [title1,title2,title3]
 print(title)
